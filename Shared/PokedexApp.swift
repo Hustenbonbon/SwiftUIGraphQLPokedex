9b2569f41da23dbc28a6a414c3da4054a406d9be
Max Tharr <max.tharr@mayflower.de>
2020-07-19 14:53:31 +0200

    Initial Commit

---
+++ Shared/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Shared
+//
+//  Created by Max Tharr on 19.07.20.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
